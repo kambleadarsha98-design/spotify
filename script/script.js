14d2e0bfa15b2bc444299004b771d185a295b3c3
kambleadarsha98@gmail.com <kambleadarsha98@gmail.com>
2025-12-16 01:10:03 +0530

    new function   add

--- script/script.js
+++ script/script.js
@@ -17,10 +17,46 @@ frisTbox.addEventListener("mouseleave",() =>{
 
 
 
-const  library  =  document.querySelector("#library"); 
+const  siderbtn =  document.querySelector("#siderbtn"); 
+const   secondbox =   document.querySelector(".secondbox"); 
+const   thirdbox =   document.querySelector(".thirdbox");
+const   library =  document.querySelector("#library");
+const  full  =   document.querySelector(".full")
+const  side =   document.querySelector(".side")
+let  boxwidth  =   true ; 
 
-library.addEventListener("mousever",()=>{
- 
-    
-});
 
+if(siderbtn &&  frisTbox){
+    siderbtn.addEventListener("click",()=>{
+    if(boxwidth){
+        fristfunction();
+     }
+     else{
+        secondfunction();
+     }
+
+     boxwidth =  !boxwidth ;
+    })
+}
+
+
+function fristfunction() {
+     
+    frisTbox.style.width  =  "10rem";
+    secondbox.style.width  =   "60rem";
+    library.style.display = "none"; 
+    full.style.display  =  "none"; 
+    siderbtn.style.marginleft   =  "50px"
+    sider .style .visibility ="visible";
+
+  
+}
+
+function secondfunction(){
+    frisTbox.style.width =   "20rem";
+    secondbox.style.width  =   "40rem"
+    library.style.display = "block";
+    full.style.display  =   "block";
+    siderbtn.style.marginleft ="40px"
+        
+}
