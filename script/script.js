a7710c82d654c937d3b993e658763a566d00607d
kambleadarsha98@gmail.com <kambleadarsha98@gmail.com>
2025-12-16 00:06:03 +0530

    new patch

---
+++ script/script.js
@@ -0,0 +1,26 @@
+const  frisTbox = document.querySelector(".fristbox"); 
+
+const   sider =   document.querySelector("#siderbtn");
+
+frisTbox.addEventListener("mouseover",()=>{
+ 
+    sider.style.visibility =   "visible";
+    
+    
+
+});
+
+frisTbox.addEventListener("mouseleave",() =>{
+  
+    sider.style.visibility =  "hidden";
+});
+
+
+
+const  library  =  document.querySelector("#library"); 
+
+library.addEventListener("mousever",()=>{
+ 
+    
+});
+
